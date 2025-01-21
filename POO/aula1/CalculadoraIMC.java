933abeb0cefcc702b6ff25630a3ea8f8ba67a07a
Marcel Tanuri <marcel.tanuri@gmail.com>
2025-01-20 23:00:21 -0300

    first commit

---
+++ POO/aula1/CalculadoraIMC.java
@@ -0,0 +1,29 @@
+public class CalculadoraIMC {
+    public static void main(String[] args) {
+        String nome = null;
+        double peso = 0.0;
+        double altura = 0.0;
+
+        for (int i = 0; i < args.length; i++) {
+            switch (args[i]) {
+                case "-n" -> nome = args[++i];
+                case "-p" -> peso = Double.parseDouble(args[++i]);
+                case "-a" -> altura = Double.parseDouble(args[++i]);
+                default -> System.out.println("Parâmetro desconhecido: " + args[i]);
+            }
+        }
+
+        if (nome == null || peso <= 0 || altura <= 0) {
+            System.out.println("Erro: Todos os parâmetros devem ser fornecidos corretamente.");
+            System.out.println("Uso: java CalculadoraIMC -n <nome> -p <peso> -a <altura>");
+            return;
+        }
+
+        double imc = peso / (altura * altura);
+
+        System.out.println("Paciente: " + nome);
+        System.out.println("Peso: " + String.format("%.2f", peso) + " kg");
+        System.out.println("Altura: " + String.format("%.2f", altura) + " m");
+        System.out.println("IMC: " + String.format("%.2f", imc));
+    }
+}
---
+++ POO/aula1/README.md
@@ -0,0 +1,11 @@
+# CalculadoraIMC
+
+compilação
+```
+javac CalculadoraIMC.java
+```
+
+exemplo de execução
+```
+java CalculadoraIMC -n "Marcel Tanuri" -a 1.88 -p 93
+```
